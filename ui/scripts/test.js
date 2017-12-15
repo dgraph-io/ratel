a921b698f4fb130b282c6e2dafd8fec8dfa54afd
Garvit Pahal <g10pahal@gmail.com>
2017-12-15 10:24:57 +0530

    Add a separate npm script for test:watch

--- ui/scripts/test.js
+++ ui/scripts/test.js
@@ -10,10 +10,4 @@ require('dotenv').config({ silent: true });
 const jest = require('jest');
 const argv = process.argv.slice(2);
 
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-    argv.push('--watch');
-}
-
-
 jest.run(argv);
