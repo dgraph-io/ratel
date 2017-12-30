2727919185fcb102e02c0d6f0f0e0fed11f47855
Garvit Pahal <g10pahal@gmail.com>
2017-12-30 19:46:58 +0530

    Import bootstrap ProgressBar from specific lib file

--- client/src/components/EntitySelector.js
+++ client/src/components/EntitySelector.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Label from "./Label";
 
 import "../assets/css/EntitySelector.scss";
--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ProgressBar } from "react-bootstrap";
+import ProgressBar from "react-bootstrap/lib/ProgressBar";
 
 import "../assets/css/Graph.scss";
 
