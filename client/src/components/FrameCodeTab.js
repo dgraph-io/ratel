72ee82ac55c1bdc5dee7073716774040015a19ad
Garvit Pahal <g10pahal@gmail.com>
2018-05-14 12:47:38 +0530

    Remove query from JSON tab as it is shown in header and is not mutable

--- client/src/components/FrameCodeTab.js
+++ client/src/components/FrameCodeTab.js
@@ -6,16 +6,6 @@ export default function FrameCodeTab({ query, response }) {
     return (
         <div className="content-container">
             <div className="code-container">
-                <div className="code-header">
-                    <span className="label label-info">Query</span>
-                </div>
-                <Highlight preClass="content">{query}</Highlight>
-            </div>
-
-            <div className="code-container">
-                <div className="code-header">
-                    <span className="label label-info">Response</span>
-                </div>
                 <Highlight preClass="content">
                     {JSON.stringify(response, null, 2)}
                 </Highlight>
