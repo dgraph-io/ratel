a2e50b1a12553d6fb8fce68bf2efdafd6911a6a1
Garvit Pahal <g10pahal@gmail.com>
2018-05-15 11:44:44 +0530

    Add copy to clipboard for json response

--- client/src/components/FrameCodeTab.js
+++ client/src/components/FrameCodeTab.js
@@ -1,15 +1,91 @@
 import React from "react";
+import Clipboard from "react-clipboard.js";
 
 import Highlight from "./Highlight";
 
-export default function FrameCodeTab({ query, response }) {
-    return (
-        <div className="content-container">
-            <div className="code-container">
-                <Highlight preClass="content">
-                    {JSON.stringify(response, null, 2)}
-                </Highlight>
+export default class FrameCodeTab extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            copyState: 0,
+        };
+
+        this._mounted = false;
+        this._timeout = null;
+    }
+
+    componentDidMount() {
+        this._mounted = true;
+    }
+
+    componentWillUnmount() {
+        this._mounted = false;
+        if (this._timeout != null) {
+            clearTimeout(this._timeout);
+            this._timeout = null;
+        }
+    }
+
+    onCopySuccess = () => {
+        this.setState({
+            copyState: 1,
+        });
+
+        if (this._timeout != null) {
+            clearTimeout(this._timeout);
+        }
+        this._timeout = setTimeout(() => {
+            if (this._mounted) {
+                this.setState({
+                    copyState: 0,
+                });
+            }
+        }, 800);
+    };
+
+    onCopyError = () => {
+        this.setState({
+            copyState: -1,
+        });
+
+        if (this._timeout != null) {
+            clearTimeout(this._timeout);
+        }
+        this._timeout = setTimeout(() => {
+            if (this._mounted) {
+                this.setState({
+                    copyState: 0,
+                });
+            }
+        }, 1500);
+    };
+
+    render() {
+        const { response } = this.props;
+        const { copyState } = this.state;
+        const json = JSON.stringify(response, null, 2);
+        return (
+            <div className="content-container">
+                <div className="code-container">
+                    <div className="code-header">
+                        <div style={{ padding: "0 6px" }}>
+                            <Clipboard
+                                data-clipboard-text={json}
+                                onSuccess={this.onCopySuccess}
+                                onError={this.onCopyError}
+                            >
+                                {copyState === 0
+                                    ? "Copy JSON Response"
+                                    : copyState > 0
+                                      ? "Copied!"
+                                      : "Error Copying!"}
+                            </Clipboard>
+                        </div>
+                    </div>
+                    <Highlight preClass="content">{json}</Highlight>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
