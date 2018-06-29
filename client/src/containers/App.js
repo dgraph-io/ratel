e8160faf6817ed5fa363cc56008e8334f2db05a1
Paul Korzhyk <paul.korzhyk@gmail.com>
2018-06-29 18:12:41 +0300

    Revert "Remove dependency on external CDN"
    This reverts commit 45f54e9c181c794db0dbc037a1f8e06f3f732816.

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -153,13 +153,9 @@ class App extends React.Component {
 
             if (queryExecutionCounter === 7) {
                 if (!readCookie("nps-survery-done")) {
-                    try {
-                        /* global delighted */
-                        delighted.survey();
-                        createCookie("nps-survery-done", true, 180);
-                    } catch (error) {
-                        console.log("Failed to call delighted.js", error);
-                    }
+                    /* global delighted */
+                    delighted.survey();
+                    createCookie("nps-survery-done", true, 180);
                 }
             } else if (queryExecutionCounter < 7) {
                 this.setState({
