cde3348f4ef3d8c7ef68d29b66642e0aa7a3e30d
Paul Korzhyk <paul.korzhyk@gmail.com>
2018-09-11 15:41:52 -0700

    Add tests to prevent breakages similar to v1.0.8

--- client/src/index.test.js
+++ client/src/index.test.js
@@ -1,8 +1,30 @@
+import jsdom from "jsdom";
 import React from "react";
 
 import AppProvider from "./containers/AppProvider";
 import App from "./containers/App";
 
-test("Rendering AppProvider should not throw errors", () => {
+import { render } from "./index";
+
+const document = new jsdom.JSDOM("<!doctype html><html><body></body></html>");
+global.document = document;
+global.window = document.defaultView;
+global.document.body.createTextRange = () => ({
+    getBoundingClientRect: () => ({ right: 0 }),
+    getClientRects: () => ({
+        left: 0,
+        length: 0,
+        right: 0,
+    }),
+    setEnd: () => {},
+    setStart: () => {},
+});
+
+test("Creating AppProvider should not throw errors", () => {
     expect(<AppProvider component={App} />).toBeTruthy();
+});
+
+test("Rendering App should not throw errors", done => {
+    render(App);
+    setTimeout(done, 2);
 });
