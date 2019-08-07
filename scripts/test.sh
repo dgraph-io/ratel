ca71be0cad25ba0de5c723ee53b448525bf4ebc9
Daniel Mai <daniel@dgraph.io>
2019-08-07 11:33:01 -0700

    Add docker-compose config for prod Ratel.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 dir="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )" && pwd )"
+clientdir="$dir/../client"
+composedir="$clientdir/src/e2etests"
 cd "$dir"
 
 cd ..
 
 ./scripts/build.prod.sh
 
-# For E2E tests: Run Ratel and Dgraph Cluster
-# Ratel: localhost:3000
-# Dgraph: localhost:8180 (HTTP)
-ratel=$(docker run --rm --detach -p 3000:3000 -v $(pwd)/build/ratel:/ratel dgraph/dgraph:latest /ratel -port=3000 -addr="http://localhost:8180")
+# Run Ratel and Dgraph
+pushd "$composedir" > /dev/null
+  docker-compose up --force-recreate --remove-orphans --detach
+popd
+./scripts/wait-for-it.sh -t 60 localhost:8080
+./scripts/wait-for-it.sh -t 60 localhost:6080
 
-(cd "$dir"; docker-compose  up --force-recreate --remove-orphans --detach)
-./scripts/wait-for-it.sh -t 60 localhost:8180
-./scripts/wait-for-it.sh -t 60 localhost:6180
-
-pushd client > /dev/null
-# Workaround: Use ?local to run production Ratel builds for e2e tests
-TEST_DGRAPH_SERVER="http://localhost:8180" TEST_RATEL_URL="http://localhost:3000?local" npm test
-testresults="$?"
+# Run tests
+pushd "$clientdir" > /dev/null
+  # Workaround: Use ?local to run production Ratel builds for e2e tests
+  TEST_DGRAPH_SERVER="http://localhost:8080" TEST_RATEL_URL="http://localhost:8000?local" npm test
+  testresults="$?"
 popd
 
-# cleanup
-(cd "$dir"; docker-compose down && docker-compose rm -f)
-docker stop $ratel
+# Cleanup
+pushd "$composedir" > /dev/null
+  docker-compose down && docker-compose rm -f
+popd
 exit $testresults
