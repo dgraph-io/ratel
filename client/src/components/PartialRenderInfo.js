0ee36cabf621af360fe3e2364adfadeaa89149fc
Paul Korzhyk <paul@dgraph.io>
2019-08-15 20:18:43 +0300

    Oops. Undo "Collapse expanded node"

--- client/src/components/PartialRenderInfo.js
+++ client/src/components/PartialRenderInfo.js
@@ -8,14 +8,14 @@
 
 import React from "react";
 
-export default function PartialRenderInfo({ remainingNodes, onExpandNetwork }) {
+export default function PartialRenderInfo({ remainingNodes, onShowMoreNodes }) {
     return (
         <div className="partial-render-info">
             Only a subset of the graph was rendered.
             <button
                 className="btn btn-link"
                 style={{ verticalAlign: "baseline" }}
-                onClick={onExpandNetwork}
+                onClick={onShowMoreNodes}
             >
                 Expand remaining {remainingNodes} nodes.
             </button>
