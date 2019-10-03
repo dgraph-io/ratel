7f20b7805fe568b187188a66a2112235ae9afc25
Daniel Mai <daniel@dgraph.io>
2019-10-03 06:06:38 -0700

    Open source Ratel under Apache 2.0 license (#126)
    * Update license script to relicense files to Apache.
    
    * Relicense Ratel files to Apache 2.0.
    
    * Update license script with current license.

--- scripts/licensing/license.sh
+++ scripts/licensing/license.sh
@@ -3,16 +3,17 @@
 tmpfile="$(mktemp)"
 dir="$(dirname "$0")"
 rateldir="$dir/../.."
-files="$(find $rateldir -type f -name "*.js" -o -name "*.go" ! -name "bindata.go")"
+files="$(find $rateldir -type f -not -path "*/client/node_modules/*" -name "*.js" -o -name "*.go" ! -name "bindata.go")"
 
 for f in $files
 do
+    sed -i -E '/\/\/ Copyright 2017-2019 Dgraph Labs, Inc. and Contributors/,/\/\/ limitations under the License\./d' $f
     cat "$dir/header.txt" $f > $tmpfile
     cp $tmpfile $f
 
     year=$(git log --format=%aD $f | tail -1 | awk '{ print $4 }')
-    if [ "$year" != "2018" ]; then
-        sed -i "s/Copyright 2018 Dgraph/Copyright $year-2018 Dgraph/g" $f
+    if [ "$year" != "2019" ]; then
+        sed -i "s/Copyright 2019 Dgraph/Copyright $year-2019 Dgraph/g" $f
     fi
 
     echo $f
