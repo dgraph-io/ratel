e1bc345b35a9d4097dda313e40190cc9a65f053b
Paul Korzhyk <paul@dgraph.io>
2019-11-11 17:39:16 +0200

    Fix #140 part 2: Migrate index.html to localStorage

--- client/public/loader.js
+++ client/public/loader.js
@@ -1,8 +1,7 @@
-var COOKIE_NAME = 'ratelAutoloadUrl';
-var COOKIE_VALUE = '?latest';
+var COOKIE_VALUE = 'latest';
 
 var cookieCheckbox = document.getElementById('cookieCheckbox');
-cookieCheckbox.checked = localStorage.getItem(COOKIE_NAME) == COOKIE_VALUE;
+cookieCheckbox.checked = localStorage.getItem(RATEL_AUTOLOAD_URL_STORAGE_KEY) == COOKIE_VALUE;
 cookieCheckbox.addEventListener('change', function onChange(evt) {
-  localStorage.setItem(COOKIE_NAME, evt.target.checked ? COOKIE_VALUE : '');
+  localStorage.setItem(RATEL_AUTOLOAD_URL_STORAGE_KEY, evt.target.checked ? COOKIE_VALUE : '');
 });
