424e6cf1710e2bc059e3ba430fa931b5b3d0aee4
Ahmed El Bannan <akelbann@gmail.com>
2020-01-25 19:01:04 -0700

    Auto zoom and fixed array mutation bug

--- client/src/components/ConsolePage/GeoView.js
+++ client/src/components/ConsolePage/GeoView.js
@@ -24,8 +24,11 @@ import {
     CircleMarker,
     Circle,
 } from "react-leaflet";
+import Leaflet from "leaflet";
 
 import "./GeoView.scss";
+
+const locationField = "location";
 
 export default function({ results }) {
     const query = useSelector(state => state.query.query);
@@ -33,7 +36,6 @@ export default function({ results }) {
     const [showOptions, setShowOptions] = useState(false);
 
     const [showLabels, setShowLabels] = useState(true);
-    const [currentZoom, setCurrentZoom] = useState(1);
     const [mapUrl, setMapUrl] = useState(
         "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json",
     );
@@ -51,7 +53,9 @@ export default function({ results }) {
 
         for (var queryKey in data) {
             if (data[queryKey] instanceof Array) {
-                parsedResults.push(...data[queryKey].filter(r => r.location));
+                parsedResults.push(
+                    ...data[queryKey].filter(r => r[locationField]),
+                );
             }
         }
 
@@ -72,7 +76,9 @@ export default function({ results }) {
     );
 
     const renderRecord = record => {
-        switch (record.location.type) {
+        const location = record[locationField];
+
+        switch (location.type) {
             case "Point":
                 return renderPoint(record);
             case "Polygon":
@@ -87,11 +93,13 @@ export default function({ results }) {
     /*
      * Creates a marker based on the location and optional label
      */
-    const renderPoint = ({ name, location }, markerColor = "blue") => {
-        const point = location.coordinates.reverse();
+    const renderPoint = (record, markerColor = "blue") => {
+        const location = record[locationField];
+        const point = location.coordinates.slice().reverse();
+
         return (
             <Marker position={point} color={markerColor}>
-                <Popup>{name}</Popup>
+                <Popup>{record.name || "Unnamed Marker"}</Popup>
             </Marker>
         );
     };
@@ -99,11 +107,13 @@ export default function({ results }) {
     /*
      * Creates a circle marker based on the location and optional label
      */
-    const renderCircleMarker = ({ name, location }, markerColor = "blue") => {
-        const point = location.coordinates.reverse();
+    const renderCircleMarker = (record, markerColor = "blue") => {
+        const location = record[locationField];
+        const point = location.coordinates.slice().reverse();
+
         return (
             <CircleMarker center={point} color={markerColor}>
-                <Popup>{name}</Popup>
+                <Popup>{record.name || "Unnamed Marker"}</Popup>
             </CircleMarker>
         );
     };
@@ -111,19 +121,15 @@ export default function({ results }) {
     /*
      * Renders a polygon
      */
-    const renderPolygon = ({ name, location }, polygonColor = "blue") => {
-        const points = location.coordinates[0].map(c => c.reverse());
-        const midpoint = points
-            .slice(0, -1)
-            .reduce((avg, point) => [avg[0] + point[0], avg[1] + point[1]], [
-                0,
-                0,
-            ])
-            .map(p => p / (points.length - 1));
+    const renderPolygon = (record, polygonColor = "blue") => {
+        const location = record[locationField];
+        const points = location.coordinates.map(a =>
+            a.map(c => c.slice().reverse()),
+        );
 
         return (
             <Polygon positions={points} color={polygonColor}>
-                <Popup>{name}</Popup>
+                <Popup>{record.name || "Unnamed Polygon"}</Popup>
             </Polygon>
         );
     };
@@ -131,11 +137,13 @@ export default function({ results }) {
     /*
      * Renders a multipolygon
      */
-    const renderMultiPolygon = ({ name, location }) => {
+    const renderMultiPolygon = record => {
+        const location = record[locationField];
         let i = 0;
+
         return location.coordinates
             .map(p => ({
-                name: name + " " + i++,
+                name: `${record.name || "Unnamed MultiPolygon"} ${i++}`,
                 location: {
                     coordinates: p,
                 },
@@ -161,7 +169,9 @@ export default function({ results }) {
                     // TODO: Render distance somehow
                     return (
                         <Circle
-                            center={JSON.parse(coordinate).reverse()}
+                            center={JSON.parse(coordinate)
+                                .slice()
+                                .reverse()}
                             radius={JSON.parse(distance)}
                             color="red"
                         >
@@ -196,11 +206,50 @@ export default function({ results }) {
         }
     };
 
+    const calculateBounds = records => {
+        let coords = [];
+
+        for (let record of records) {
+            const location = record[locationField];
+
+            switch (location.type) {
+                case "Point":
+                    coords.push(
+                        Leaflet.latLng(location.coordinates.slice().reverse()),
+                    );
+                    break;
+
+                /*case "Polygon":
+                    for (let points of location.coordinates) {
+                        for (let c of points) {
+                            coords.push(Leaflet.latLng(c.reverse()));
+                        }
+                    }
+                    break;
+
+                case "MultiPolygon":
+                    for (let polygon of location.coordinates) {
+                        for (let points of polygon) {
+                            for (let c of points) {
+                                coords.push(Leaflet.latLng(c.reverse()));
+                            }
+                        }
+                    }
+                    break; */
+            }
+        }
+        console.log(coords);
+        return Leaflet.latLngBounds(coords);
+    };
+
     const handleClose = () => setShowOptions(false);
     const handleShow = () => setShowOptions(true);
 
     // Render starts here
     const locations = parseResults(results);
+    const bounds =
+        locations.length > 0 ? calculateBounds(locations) : undefined;
+    const center = locations.length > 0 ? bounds.getCenter() : [0, 0];
 
     return (
         <>
@@ -219,11 +268,16 @@ export default function({ results }) {
                 {/* Usage instructions */}
                 {locations.length === 0 && renderInstructions()}
 
-                <Map zoom={2} maxZoom={19} center={[0, 0]} className="map">
+                <Map
+                    zoom={19}
+                    maxZoom={19}
+                    center={center}
+                    bounds={bounds}
+                    className="map"
+                >
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        noWrap={true}
                     />
                     {/* Render records */}
                     {locations.map(renderRecord)}
