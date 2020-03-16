d09500ac7e1f4fb95da429fe74385f4f13fae328
Paul Korzhyk <paul@dgraph.io>
2020-03-16 17:29:19 +0200

    Roll me back

--- client/src/e2etests/acl/aclHelpers.js
+++ client/src/e2etests/acl/aclHelpers.js
@@ -69,6 +69,7 @@ export const loginUser = async (
     await waitForElementDisappear(page, spinnerSelector);
 
     const sidebarText = await getElementText(page, `.sidebar-content.open`);
+    console.log("sidebarText at end is ", sidebarText);
     return sidebarText.includes(`Logged in as "${userid}"`);
 };
 
