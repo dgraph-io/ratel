35e673d7f741312a25cce30fa1426abf043a4210
Paul Korzhyk <paul@dgraph.io>
2020-04-22 17:59:33 +0300

    fix formatting

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -27,21 +27,21 @@ function buildClient {
 }
 
 function doChecks {
-    if ! hash go 2>/dev/null; then
-		echo "Could not find golang. Please install Go env and try again.";
-		exit 1;
-	fi
+  if ! hash go 2>/dev/null; then
+    echo "Could not find golang. Please install Go env and try again.";
+    exit 1;
+  fi
 
-    if hash go-bindata 2>/dev/null; then
-        go_bindata="$(which go-bindata)"
-       else
-        echo "Could not find go-bindata.";
-        echo "Trying to install go-bindata. If it fails, please read the INSTRUCTIONS.md";
-        go get github.com/jteeuwen/go-bindata/go-bindata
-        sleep 2
-        go_bindata="$(which go-bindata)"
-        exit 0;
-    fi
+  if hash go-bindata 2>/dev/null; then
+    go_bindata="$(which go-bindata)"
+  else
+    echo "Could not find go-bindata.";
+    echo "Trying to install go-bindata. If it fails, please read the INSTRUCTIONS.md";
+    go get github.com/jteeuwen/go-bindata/go-bindata
+    sleep 2
+    go_bindata="$(which go-bindata)"
+    exit 0;
+  fi
 }
 
 # Build server files.
@@ -67,7 +67,7 @@ function buildServer {
 
     # This is necessary, as the go build flag "-ldflags" won't work with spaces.
     escape=$(echo $commitINFO | sed -e "s/ /¨•¨/g")
-    
+
     ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.commitINFO=$escape"
     ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.commitID=$commitID"
 
