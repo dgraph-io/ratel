21285e6e49b7314201ee32dfd3196cc3ae6a01a0
Michel Conrado <MichelDiz@users.noreply.github.com>
2020-04-22 20:02:12 -0300

    add cobra and basic scripts changes

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -65,11 +65,9 @@ function buildServer {
         ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.version=$2"
     fi
 
-    # This is necessary, as the go build flag "-ldflags" won't work with spaces.
-    escape=$(echo $commitINFO | sed -e "s/ /¨•¨/g")
-
-    ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.commitINFO=$escape"
-    ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.commitID=$commitID"
+    ldflagsVal="$ldflagsVal -X 'github.com/dgraph-io/ratel/server.branch=$gitBranch'"
+    ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.lastCommitSHA1=$lastCommitSHA1"
+    ldflagsVal="$ldflagsVal -X 'github.com/dgraph-io/ratel/server.lastCommitTime=$lastCommitTime'"
 
     # Get packages before build
     go get ./
