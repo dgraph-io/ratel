cc64a82434f41935a00b4055946292a5ce2fcd74
Prashant Shahi <prashant@dgraph.io>
2020-04-28 23:18:51 +0530

    Changes to the build and test scripts
    - Modified docker-compose.prod.yml to include the latest dgraph binary
    - Replaced cd with pushd and popd
    - Added comments and indentations
    - Ran the script throught shellcheck.net and passed
    
    Signed-off-by: Prashant Shahi <prashant@dgraph.io>

--- scripts/build.prod.sh
+++ scripts/build.prod.sh
@@ -2,39 +2,43 @@
 
 set -e
 
-version="$(cat ../client/package.json | grep -i "version*" | awk -F '"' '{print $4}')" 
-flagUploadToS3=false
-commitID="$(git rev-parse --short HEAD)"
-commitINFO="$(git show --pretty=format:"%h  %ad  %d" | head -n1)"
+dir="$( dirname "${BASH_SOURCE[0]}" )"
+rootdir="$dir/.."
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -v | --version )    shift
-                            version=$1
-                            ;;
-        -u | --upload )     flagUploadToS3=true
-                            ;;
-    esac
+# cd to the scripts directory
+pushd "$dir" > /dev/null
+    # setting metadata and flags
+    version="$(grep -i '"version"' < "$rootdir/client/package.json" | awk -F '"' '{print $4}')"
+    flagUploadToS3=false
+    commitID="$(git rev-parse --short HEAD)"
+    commitINFO="$(git show --pretty=format:"%h  %ad  %d" | head -n1)"
 
-    shift
-done
+    while [ "$1" != "" ]; do
+        case $1 in
+            -v | --version )    shift
+                                version=$1
+                                ;;
+            -u | --upload )     flagUploadToS3=true
+                                ;;
+        esac
 
-PREV="$( pwd )"
+        shift
+    done
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-source ./functions.sh
+popd > /dev/null
 
 # cd to the root folder.
-cd ..
+pushd "$rootdir" > /dev/null
+    # build client - production flag set to true
+    buildClient true
 
-buildClient true
-buildServer true $version
+    # build server - passing along the production flag and version
+    buildServer true "$version"
 
-if [ $flagUploadToS3 = true ]; then
-    uploadToS3
-fi
+    # uploading to s3 when the flagUploadToS3 flag set to true
+    if [ $flagUploadToS3 = true ]; then
+        uploadToS3
+    fi
+popd > /dev/null
 
-echo
-echo "DONE"
-
-cd $PREV
+printf "\nDONE\n"
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -2,71 +2,66 @@
 
 # Build client files.
 function buildClient {
-    echo
-    echo "=> Building client files..."
+    printf "\n=> Building client files..."
+    # cd to client directory
+    pushd client
+        # Install all or missing dependencies.
+        if hash yarn 2>/dev/null; then
+            # if yarn is installed use it. much faster than npm
+            yarn install
+        else
+            npm install
+        fi
 
-    cd client
-
-    # Install all or missing dependencies.
-    if hash yarn 2>/dev/null; then
-        # if yarn is installed use it. much faster than npm
-        yarn install
-    else
-        npm install
-    fi
-
-    # Check if production build.
-    if [ $1 = true ]; then
-        npm run build:prod
-    else
-        npm run build:local
-    fi
-
+        # Check if production build.
+        if [ "$1" = true ]; then
+            npm run build:prod
+        else
+            npm run build:local
+        fi
     # cd to root directory.
-    cd ..
+    popd > /dev/null
 }
 
 function doChecks {
     if ! hash go 2>/dev/null; then
-		echo "Could not find golang. Please install Go env and try again.";
+		printf "Could not find golang. Please install Go env and try again.";
 		exit 1;
 	fi
 
     if hash go-bindata 2>/dev/null; then
         go_bindata="$(which go-bindata)"
-       else
-        echo "Could not find go-bindata.";
-        echo "Trying to install go-bindata. If it fails, please read the INSTRUCTIONS.md";
+    else
+        printf "Could not find go-bindata. Make sure ratel is in the GOPATH and GOBIN environment variable is set.";
+        printf "Trying to install go-bindata. If it fails, please read the INSTRUCTIONS.md.";
         go get github.com/jteeuwen/go-bindata/go-bindata
         sleep 2
         go_bindata="$(which go-bindata)"
-        exit 0;
     fi
 }
 
 # Build server files.
 function buildServer {
     doChecks
-    echo
-    echo "=> Building server files..."
+    printf "\n=> Building server files..."
 
     # Run bindata for all files in in client/build/ (recursive).
     $go_bindata -o ./server/bindata.go -pkg server -prefix "./client/build" -ignore=DS_Store ./client/build/...
 
     # Check if production build.
-    if [ $1 = true ]; then
+    if [ "$1" = true ]; then
         ldflagsVal="-X github.com/dgraph-io/ratel/server.mode=prod"
     else
         ldflagsVal="-X github.com/dgraph-io/ratel/server.mode=local"
     fi
 
     # Check if second argument (version) is present and not empty.
-    if [ -n $2 ]; then
+    if [ -n "$2" ]; then
         ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.version=$2"
     fi
 
     # This is necessary, as the go build flag "-ldflags" won't work with spaces.
-    escape=$(echo $commitINFO | sed -e "s/ /¨•¨/g")
+    escape="$(printf '%s' "$commitINFO" | sed -e "s/ /¨•¨/g")"
     
     ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.commitINFO=$escape"
     ldflagsVal="$ldflagsVal -X github.com/dgraph-io/ratel/server.commitID=$commitID"
@@ -77,8 +72,7 @@ function buildServer {
 
 # Upload client static files to AWS S3.
 function uploadToS3 {
-    echo
-    echo "=> Uploading client static files to AWS S3..."
+    printf "\n=> Uploading client static files to AWS S3..."
     aws s3 cp --recursive ./client/build/static s3://dgraph-io-ratel/dev/static
     aws s3 cp --recursive ./client/build/static s3://dgraph-io-ratel/static
     aws cloudfront create-invalidation --distribution-id EJF7H0N2C94FP --paths "/*"
