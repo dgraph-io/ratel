e82571582491dd79a9cd50243ba59b03a357c780
Paul Korzhyk <paul@dgraph.io>
2020-06-01 18:47:16 +0300

    Support & persist query variables in reducer

--- client/src/reducers/query.js
+++ client/src/reducers/query.js
@@ -12,55 +12,57 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import produce from "immer";
 import {
     UPDATE_QUERY,
     UPDATE_ACTION,
     UPDATE_QUERY_AND_ACTION,
+    UPDATE_QUERY_VARS,
     UPDATE_READ_ONLY,
     UPDATE_BEST_EFFORT,
 } from "../actions/query";
 
 const defaultState = {
     query: "",
+    queryVars: [],
     allQueries: { query: "", mutate: "", alter: "" },
     action: "query",
     readOnly: true,
     bestEffort: true,
 };
 
-export default function query(state = defaultState, action) {
-    switch (action.type) {
-        case UPDATE_QUERY:
-            return {
-                ...state,
-                query: action.query,
-            };
-        case UPDATE_ACTION:
-            state.allQueries[state.action] = state.query || "";
-            return {
-                ...state,
-                query: state.allQueries[action.action] || "",
-                action: action.action,
-            };
-        case UPDATE_QUERY_AND_ACTION:
-            state.allQueries[state.action] = state.query || "";
-            return {
-                ...state,
-                query: action.query,
-                action: action.action,
-            };
-        case UPDATE_READ_ONLY:
-            return {
-                ...state,
-                readOnly: action.readOnly,
-                bestEffort: action.readOnly,
-            };
-        case UPDATE_BEST_EFFORT:
-            return {
-                ...state,
-                bestEffort: action.bestEffort,
-            };
-        default:
-            return state;
-    }
-}
+export default (state = defaultState, action) =>
+    produce(state, draft => {
+        draft.allQueries = draft.allQueries || {};
+        draft.action = draft.action || "query";
+        draft.allQueries[draft.action] = draft.query || "";
+
+        switch (action.type) {
+            case UPDATE_QUERY:
+                draft.query = action.query;
+                break;
+
+            case UPDATE_ACTION:
+                draft.action = action.action;
+                draft.query = draft.allQueries[draft.action];
+                break;
+
+            case UPDATE_QUERY_AND_ACTION:
+                draft.action = action.action;
+                draft.query = action.query;
+                break;
+
+            case UPDATE_READ_ONLY:
+                draft.readOnly = action.readOnly;
+                draft.bestEffort = action.readOnly;
+                break;
+            case UPDATE_BEST_EFFORT:
+                draft.bestEffort = action.readOnly;
+                break;
+
+            case UPDATE_QUERY_VARS:
+                draft.queryVars = action.newVars;
+            default:
+                break;
+        }
+    });
