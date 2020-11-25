2588afe726e669bd70abfc4fb7a2fc9c6e030549
Michel Diz <michelconrado@outlook.com>
2020-11-25 00:30:03 -0300

    small adjustments in the Copyright

--- scripts/licensing/license.sh
+++ scripts/licensing/license.sh
@@ -7,13 +7,13 @@ files="$(find $rateldir -type f -not -path "*/client/node_modules/*" -name "*.js
 
 for f in $files
 do
-    sed -i -E '/\/\/ Copyright 2017-2019 Dgraph Labs, Inc. and Contributors/,/\/\/ limitations under the License\./d' $f
+    sed -i -E '/\/\/ Copyright 2017-2021 Dgraph Labs, Inc. and Contributors/,/\/\/ limitations under the License\./d' $f
     cat "$dir/header.txt" $f > $tmpfile
     cp $tmpfile $f
 
     year=$(git log --format=%aD $f | tail -1 | awk '{ print $4 }')
     if [ "$year" != "2019" ]; then
-        sed -i "s/Copyright 2019 Dgraph/Copyright $year-2019 Dgraph/g" $f
+        sed -i "s/Copyright 2017-2021 Dgraph/Copyright $year-2019 Dgraph/g" $f
     fi
 
     echo $f
