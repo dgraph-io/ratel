621e7c752b41d1258494106be9c4d8ab71b88a41
Daniel Mai <daniel@dgraph.io>
2021-03-25 12:46:54 -0700

    build: Create main.css.

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -655,8 +655,8 @@ module.exports = function (webpackEnv) {
         new MiniCssExtractPlugin({
           // Options similar to the same options in webpackOptions.output
           // both options are optional
-          filename: 'static/css/[name].[contenthash:8].css',
-          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+          filename: 'static/css/[name].css'
+          // chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
         }),
       // Generate an asset manifest file with the following content:
       // - "files" key: Mapping of all asset filenames to their corresponding
