839072b559c69ecc931e5085e41ed93e7a2f46ef
Michel Diz <MichelDiz@users.noreply.github.com>
2022-11-07 02:24:09 -0300

    upgrade some packages(tested)

--- dev/run.sh
+++ dev/run.sh
@@ -5,13 +5,13 @@ set -e
 # Fix files for Windows. Converting the files to unix type.
 # This is necessary if you are developing from Windows and will be working in a Linux container. 
 # Because the file type is not compatible with Linux if it is on a Windows disk.
-find ./* -type d \( -path *node_modules/* -o -path ./.git -o -path */3rdpartystatic/* \) -prune -o -name '*.js*' -print0 | xargs -0 dos2unix
-find ./* -type d \( -path *node_modules/* -o -path ./.git -o -path */3rdpartystatic/* \) -prune -o -name '*.sh*' -print0 | xargs -0 dos2unix
+# find ./* -type d \( -path *node_modules/* -o -path ./.git -o -path */3rdpartystatic/* \) -prune -o -name '*.js*' -print0 | xargs -0 dos2unix
+# find ./* -type d \( -path *node_modules/* -o -path ./.git -o -path */3rdpartystatic/* \) -prune -o -name '*.sh*' -print0 | xargs -0 dos2unix
 
 cd ./client
 
 npm cache clean --force
 
-npm install --legacy-peer-deps
+npm install --legacy-peer-deps --no-optional
 
 npm run start
