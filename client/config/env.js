15a6443c199b1145c27e5acf09948f6c1e22b8ac
Matthew McNeely <matthew.mcneely@gmail.com>
2026-01-26 13:24:38 -0500

    Fix favicon loading

--- client/config/env.js
+++ client/config/env.js
@@ -76,6 +76,11 @@ function getClientEnvironment() {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
         NODE_ENV: process.env.NODE_ENV || 'development',
+        // Useful for resolving the correct path to static assets in `public`.
+        // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
+        // This should only be used as an escape hatch. Normally you would put
+        // images into the `src` and `import` them in code to get their paths.
+        PUBLIC_URL: paths.publicUrlOrPath.slice(0, -1),
       },
     )
 
